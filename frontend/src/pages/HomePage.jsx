6e91c630d4571131a526c092dbb361dc2f5818c5
Aleiaco02 <alessandroiacovelli83@gmail.com>
2026-03-19 20:49:52 +0100

    setup pc

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useMemo, useRef, useState } from "react";
-import { Link } from "react-router-dom";
 import { getProducts } from "../services/api";
 import { useNavigate } from "react-router-dom";
 
